a5d76fe9708c6475b2b9a21721f657f8b4fdb9cf
sean-code <john.nganga@student.moringaschool.com>
2023-03-29 12:53:15 +0300

    Create: Highlight the backdrop and modal class

---
+++ src/components/Modal.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class='backdrop'>
+        <div class="model">
+            <p>Modal Content</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.modal{
+    width: 480px;
+    padding: 20px;
+    margin: 100px auto;
+    background: #fafafa;
+    border-radius: 10px;
+}
+
+.backdrop{
+    top: 0;
+    position: fixed;
+    background: rgba(0, 0, 0, 0.5);
+    width: 100%;
+    height: 100%;
+}
+
+</style>
